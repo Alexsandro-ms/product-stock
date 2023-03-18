b8623a81bb148b553a82b8199117bab563dbc298
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-03-17 21:21:15 -0300

    feat: List route of all users created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { create, get, getById } from "../controllers/user.controller.js";
 
 export const userRoutes = (app) => {
-  app.get("/", (req, res) => res.json({}));
+  app.post("/user", create);
+  app.get("/user", get);
+  app.get("/user/:id", getById);
 };
