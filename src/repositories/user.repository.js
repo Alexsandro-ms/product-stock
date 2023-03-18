373e521af96f8418898a693cffdf796658f74c1e
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-03-17 21:19:56 -0300

    feat: functionality of list of all users created

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -16,3 +16,21 @@ export const createUser = async (data) => {
 
   return user;
 };
+
+export const get = async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    return res.status(200).send(users);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+export const getById = async (req, res) => {
+  try {
+    const user = await getById(Number(req.params.id));
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
