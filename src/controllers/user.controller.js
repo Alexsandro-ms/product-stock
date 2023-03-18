cac54fae5692a9e91aaf724ee2f8c81feaa27ba5
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-03-17 21:20:48 -0300

    feat: controller for list of all users created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { createUser } from "../repositories/user.repository.js";
+import { createUser, getAllUsers } from "../repositories/user.repository.js";
 import { userValidation } from "../validation/user.validation.js";
 import bcrypt from "bcrypt";
 
@@ -10,6 +10,24 @@ export const create = async (req, res) => {
 
     req.body.password = hashPassword;
     const user = await createUser(req.body);
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+export const get = async (req, res) => {
+  try {
+    const users = await getAllUsers();
+    return res.status(200).send(users);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+export const getById = async (req, res) => {
+  try {
+    const user = await getById(Number(req.params.id));
     return res.status(200).send(user);
   } catch (error) {
     return res.status(400).send(error);
