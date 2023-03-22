30ef98f814fb6dd6aae163ba6e8ba68c4958bbb5
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-03-21 21:14:41 -0300

    feat: added parameter validation.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,9 +31,20 @@ export const get = async (req, res) => {
 
 export const getId = async (req, res) => {
   try {
-    const user = await getById(Number(req.params.id));
+    const id = req.params.id;
+
+    if (!Number(id)) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const user = await getById(Number(id));
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
     return res.status(200).send(user);
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(500);
   }
 };
