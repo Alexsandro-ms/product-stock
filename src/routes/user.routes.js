87de1c26a04527f1817a625f37bf20d385be8a9f
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2023-03-23 16:17:28 -0300

    feat: route to remove user created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
-import { create, get, getId, update } from "../controllers/user.controller.js";
+import {
+  create,
+  get,
+  getId,
+  remove,
+  update
+} from "../controllers/user.controller.js";
 
 export const userRoutes = (app) => {
   app.post("/user", create);
   app.get("/user", get);
   app.get("/user/:id", getId);
   app.put("/user/:id", update);
+  app.delete("/user/:id", remove);
 };
